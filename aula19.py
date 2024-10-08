05d1d77a365b9df096242875bdfb16066beedc26
Eloyse <sopraestudoeloyse@gmail.com>
2024-10-08 10:50:04 -0300

    Add files via upload

---
+++ aula19.py
@@ -0,0 +1,11 @@
+condicao = True
+
+while condicao:
+      nome = input('Qual seu nome: ')
+
+      if nome == 'sair':
+         break
+      
+      print(f'seu nome é {nome}')
+
+      print('acabou')
