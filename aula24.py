657899937b3f36a07e85c155bfe33b5730a60d4d
Eloyse <sopraestudoeloyse@gmail.com>
2024-10-17 10:56:06 -0300

    Add files via upload

---
+++ aula24.py
@@ -0,0 +1,10 @@
+# Eloyse Gomes 2C
+
+# Solicita ao usuário um número
+numero = int(input("Digite um número para ver sua tabuada: "))
+
+# Exibe a tabuada do número fornecido
+print(f"Tabuada do {numero}:")
+for i in range(1, 11):  # de 1 a 10
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
